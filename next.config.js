043e40936735cf9ec6bbfe5436abbd5604aeb31e
Jose Mata <mata649@hotmail.com>
2023-03-30 01:10:31 -0600

    feat: api host env var

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  publicRuntimeConfig:{
-	apiURL: 'http://localhost:5000'
-  },
-  compiler:{
-	styledComponents:true
-  }
+	reactStrictMode: false,
+	swcMinify: true,
+	publicRuntimeConfig: {
+		apiURL: process.env["API_HOST"] ?? 'http://localhost:5000'
+	},
+	compiler: {
+		styledComponents: true
+	}
 }
 
 module.exports = nextConfig
