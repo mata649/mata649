c22461a944353cc3be3103496a9d732116125942
Jose Mata <mata649@hotmail.com>
2023-03-30 16:50:52 -0600

    aaa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config()
 
 const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
 	publicRuntimeConfig: {
-		apiURL: process.env["API_HOST"] ?? 'http://localhost:5000'
+		apiURL: process.env.API_HOST ?? 'http://localhost:5000'
 	},
 	compiler: {
 		styledComponents: true
