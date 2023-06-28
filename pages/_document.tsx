3d8569644e2820c1715d9548aaf28d42fc091bf7
Jose Mata <mata649@hotmail.com>
2023-06-28 17:35:19 -0600

    fix: document title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html >
-			<Head />
+			<Head >
+				<title>💾 J0S3 M4T4</title>
+			</Head >
 			<body className='bg-back-light dark:bg-back-dark' >
 				<Main />
 				<NextScript />
